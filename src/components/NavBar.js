a05054520dbe2c59290c694e766bd3cd3c83fe79
WillHDev <will.v.holcombe@gmail.com>
2021-09-17 18:59:52 -0600

    added basic html

---
+++ src/components/NavBar.js
@@ -0,0 +1,106 @@
+import {
+
+    Link
+  } from "react-router-dom";
+
+const Navbar = () => {
+
+    return (
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+          {/* <Link to="/"> */}
+          <a className="navbar-item" >
+              <h1>Content Manager</h1>
+            </a>
+              {/* </Link> */}
+            <span className="navbar-burger burger" data-target="navbarMenu">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+          <div id="navbarMenu" className="navbar-menu">
+            <div className="navbar-end">
+              <div className=" navbar-item">
+                <div className="control has-icons-left">
+                  <input className="input is-rounded" type="email" placeholder="Search" />
+                  <span className="icon is-left">
+                    <i className="fa fa-search"></i>
+                  </span>
+                </div>
+              </div>
+              {/* <Link to="/"> */}
+              <a className="navbar-item is-active is-size-5 has-text-weight-semibold">
+                Home
+              </a>
+              {/* </Link> */}
+
+              {/* <Link to="/resources/new"> */}
+              <a
+                  className="navbar-item is-size-5 has-text-weight-semibold">
+                Add
+              </a>
+              {/* </Link> */}
+              <a className="navbar-item is-size-5 has-text-weight-semibold">
+                Features
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+  
+  export default Navbar;
+
+//the browser is refreshed in order to request a new
+//page
+
+//next allows it to happen much faster and without a refresh
+// in the browser
+//
+
+// const NavBar = (params) => {
+//     return ( 
+//             <nav classNameName="navbar">
+//             <div classNameName="container">
+//             <div classNameName="navbar-brand">
+//                 <a classNameName="navbar-item" href="../">
+//                 <img src="../images/bulma.png" alt="Logo"/>
+//                 </a>
+//                 <span classNameName="navbar-burger burger" data-target="navbarMenu">
+//                 <span></span>
+//                 <span></span>
+//                 <span></span>
+//                 </span>
+//             </div>
+//             <div id="navbarMenu" classNameName="navbar-menu">
+//                 <div classNameName="navbar-end">
+//                 <div classNameName=" navbar-item">
+//                     <div classNameName="control has-icons-left">
+//                     <input classNameName="input is-rounded" type="email" placeholder="Search" />
+//                     <span classNameName="icon is-left">
+//                         <i classNameName="fa fa-search"></i>
+//                     </span>
+//                     </div>
+//                 </div>
+//                 <a classNameName="navbar-item is-active is-size-5 has-text-weight-semibold">
+//                     Home
+//                 </a>
+//                 <a classNameName="navbar-item is-size-5 has-text-weight-semibold">
+//                     Examples
+//                 </a>
+//                 <a classNameName="navbar-item is-size-5 has-text-weight-semibold">
+//                     Features
+//                 </a>
+//                 </div>
+//             </div>
+//             </div>
+//             </nav>
+
+ 
+
+//     )
+// }
+// export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div> <App /></div>
+     ,
+  
   document.getElementById('root')
 );
 
@@ -15,3 +21,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
