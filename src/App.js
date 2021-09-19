54981c4b89826c54d284b1ef28eb505078e67786
WillHDev <will.v.holcombe@gmail.com>
2021-09-18 20:39:38 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 // import './App.css';
+import React, { useState, useEffect } from 'react'
+import moduleName from 'module'
 import 'bulma/css/bulma.min.css';
 import './styles/globals.css'
 import Layout from './components/Layout'
@@ -9,7 +11,51 @@ import NewsLetter from './components/NewsLetter'
 import Footer from './components/Footer'
 
 
-function App() {
+const data2 = [
+  {
+      id: "1",
+      title: "Learning Singleton Pattern",
+      description: "I would like to learn singleton in JS language because it's important for my job",
+      link: "https://link.com",
+      proiority: 3,
+      timeToFinish: 120,
+    status: "active"
+  },
+  {
+      id: "2",
+      title: "Resouce 2 Description",
+      description: "I would like to learn singleton in JS language because it's important for my job",
+      link: "https://link.com",
+      proiority: 2,
+      timeToFinish: 60,
+    status: "inactive"
+  }
+];
+
+
+
+async function fetchResourcesJSON() {
+  const response = await fetch("http://localhost:8080/api/resources");
+  const resources = await response.json();
+  console.log(resources)
+  return resources;
+}
+
+function App( ) {
+  const [resources] = useState([]);
+
+  useEffect(() => {
+    // You need to restrict it at some point
+    // This is just dummy code and should be replaced by actual
+      const data = fetchResourcesJSON();
+      return data;
+  }, []);
+
+ 
+  console.log(resources);
+
+  //
+  
   return (
    
     <div className="App">
@@ -17,10 +63,10 @@ function App() {
       <header className="App-header">
       <>
         <Layout>
-        {/* <ResourceHighlight
+        {/* <ResourceHighlight */}
         
-        />   */}
-        <NewsLetter />
+        {/* />    */}
+        {/* <NewsLetter /> */}
         {/* <ResourceList 
          
         /> */}
@@ -33,6 +79,26 @@ function App() {
    
   );
 }
+
+
+
+
+
+// export async function getServerSideProps() {
+//   //write async in front of the function where you would
+//   //like to resolve this data
+
+//   const resData = await fetch("http://localhost:8080/api/resources");
+//   //********* */
+  
+//   const data = await resData.json();
+//   //only appears in terminal
+ 
+//      return {  props: {
+//        resources: data
+//      }
+//     }
+// }
 
 export default App;
 // resources={resources.slice(2)}
